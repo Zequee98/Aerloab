706a84941656ad7930118921a40bb20baafb5565
Zequee98 <ezequiel_grox@hotmail.com>
2017-12-30 19:49:23 -0300

    Configuracion inical y estructura de carpetas termindas

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path= require ('path');
+
+module.exports = {
+  entry: [
+    './src/app.js'
+  ],
+  module: {
+    loaders: [{
+      test: /(\.js|\.jsx)$/,
+      loader: "babel-loader",
+      exclude: /node_modules/,
+      query: { presets: ['env', 'stage-0', 'react'] },
+    }]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, '/public/dist'),
+  },
+  devtool: "source-map",
+  // resolve: {
+  //   extensions: ['.js', '.jsx']
+  // },
+};
